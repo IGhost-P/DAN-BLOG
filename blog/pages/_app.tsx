065a6931294117630eb4a5213b1f86ccc8e8d415
yangsnagwoo <dndb3599@gmail.com>
2022-08-22 20:52:27 +0900

    init : nextJs - TS

---
+++ blog/pages/_app.tsx
@@ -0,0 +1,19 @@
+// pages/_app.tsx
+import { css, Global } from "@emotion/react";
+import type { AppProps } from "next/app";
+import { reset } from "styles/reset";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Global
+        styles={css`
+          ${reset}
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
