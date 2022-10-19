161ce02cfa305c85863a438fe7cfc43a1449797e
sangwooyang <dndb3599@gmail.com>
2022-10-19 16:48:31 +0900

    feat : 로고 클릭시 루트 페이지로 라우트

--- components/atom/Logo/index.tsx
+++ components/atom/Logo/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useRouter } from "next/router";
 import LogoSvg from "assets/images/Logo.svg";
 import LogoDarkSvg from "assets/images/Logo-dark.svg";
 import Image from "next/image";
@@ -7,14 +8,17 @@ import { lightTheme } from "styles/theme";
 import * as S from "./style";
 export function Logo() {
   const { colorTheme, toggleColorTheme } = useContext(ThemeContext);
+  const router = useRouter();
 
   return (
     <div className="logo" css={S.Container}>
       <Image
+        onClick={() => router.push("/")}
         src={colorTheme === lightTheme ? LogoSvg : LogoDarkSvg}
         alt="logo"
         width={437}
         height={89}
+        style={{ cursor: "pointer" }}
       />
     </div>
   );
